01741acdc65a90433f94730a20658453777ef0fb
Andre Popovitch <andre@popovit.ch>
2019-09-16 23:23:46 -0400

    got it working!

--- Assets/WaveformcCollapse.cs
+++ Assets/WaveformcCollapse.cs
@@ -5,16 +5,16 @@ using System.Linq;
 
 public class WaveformcCollapse : MonoBehaviour
 {
-    public int n = 3;
+    public int n = 2;
     public int maxVal = 1;
     public int outputSizeX = 20;
     public int outputSizeY = 20;
+    
     public List<List<int>> input = new List<List<int>>  {
-        new List<int> { 1, 1, 1, 1, 1 },
-        new List<int> { 1, 0, 0, 0, 0 },
-        new List<int> { 1, 0, 1, 0, 0 },
-        new List<int> { 1, 0, 0, 0, 0 },
-        new List<int> { 1, 0, 0, 0, 0 },
+        new List<int> { 0, 0, 0, 0, },
+        new List<int> { 0, 1, 1, 1, },
+        new List<int> { 0, 1, 0, 1, },
+        new List<int> { 0, 1, 1, 1, },
     };
 
     private Dictionary<List<List<int>>, int> patterns;
@@ -27,34 +27,38 @@ public class WaveformcCollapse : MonoBehaviour
     int offsetx = 0;
     int offsety = 0;
     int steps = 0;
-
+    bool contradictive = false;
     // Start is called before the first frame update
     void Start()
     {
+        transpose(input);
         Random.seed = 42;
         squares = new List<GameObject>();
         patterns = new Dictionary<List<List<int>>, int>();
         wave = new List<List<Dictionary<List<List<int>>, bool>>>();
-        foreach (int x in Enumerable.Range(0, input.Count - n + 1))
+        foreach (var permutation in new List<List<List<int>>> { input, rotate90(input), rotate180(input), rotate270(input), flip(input), flip(rotate90(input)) })
         {
-            foreach (int y in Enumerable.Range(0, input[0].Count - n + 1))
+            foreach (int x in Enumerable.Range(0, permutation.Count - n + 1))
             {
-                var l = new List<List<int>>();
-                foreach (int xi in Enumerable.Range(0, n))
+                foreach (int y in Enumerable.Range(0, permutation[0].Count - n + 1))
                 {
-                    l.Add(new List<int>());
-                    foreach (int yi in Enumerable.Range(0, n))
+                    var l = new List<List<int>>();
+                    foreach (int xi in Enumerable.Range(0, n))
                     {
-                        l[xi].Add(input[x + xi][y + yi]);
+                        l.Add(new List<int>());
+                        foreach (int yi in Enumerable.Range(0, n))
+                        {
+                            l[xi].Add(permutation[x + xi][y + yi]);
+                        }
                     }
-                }
-                if (patterns.ContainsKey(l))
-                {
-                    patterns[l] += 1;
-                }
-                else
-                {
-                    patterns[l] = 1;
+                    if (patterns.ContainsKey(l))
+                    {
+                        patterns[l] += 1;
+                    }
+                    else
+                    {
+                        patterns[l] = 1;
+                    }
                 }
             }
         }
@@ -108,11 +112,13 @@ public class WaveformcCollapse : MonoBehaviour
 
     public void step()
     {
+        if (contradictive) return;
         steps++;
         void detectContradictions(List<List<Dictionary<List<List<int>>, bool>>> wave, out bool d, out int mx, out int my)
         {
             // Find the position with the most negentropy
             d = true;
+            var ps = new List<Vector2>();
             mx = 0;
             my = 0;
             var maxscore = 0;
@@ -136,17 +142,31 @@ public class WaveformcCollapse : MonoBehaviour
                     }
                     if (numberOfTrues == 0)
                     {
+                        contradictive = true;
                         Debug.Log("We have encountered a contradiction at " + x.ToString() +", " + y.ToString() + "!");
                     }
-                    if (numberOfTrues > 1 && (!choiceIsValid || numberOfFalses > maxscore))
+                    if (numberOfTrues > 1)
                     {
-                        d = false;
-                        mx = x;
-                        my = y;
-                        maxscore = numberOfFalses;
-                        choiceIsValid = true;
+                        if (!choiceIsValid || numberOfFalses > maxscore)
+                        {
+                            d = false;
+                            ps = new List<Vector2>();
+                            ps.Add(new Vector2(x, y));
+                            maxscore = numberOfFalses;
+                            choiceIsValid = true;
+                        }
+                        else if (numberOfFalses == maxscore)
+                        {
+                            ps.Add(new Vector2(x, y));
+                        }
                     }
                 }
+            }
+            if (choiceIsValid)
+            {
+                var v = ps[Random.Range(0, ps.Count)];
+                mx = (int)v.x;
+                my = (int)v.y;
             }
         }
         int maxx;
@@ -158,39 +178,62 @@ public class WaveformcCollapse : MonoBehaviour
         if (!done)
         {
             // collapse the superposition
-            var oneToKeep = Random.Range(0, wave[maxy][maxx].Count - 1);
-            int i = -1;
-            bool alreadySet = false;
+
+            Debug.Log("collapsing wavefunction at " + maxx.ToString() + ", " + maxy.ToString());
+            if (maxx == 8 && maxy == 1)
+            {
+                Debug.Log("!!!");
+            }
+            var onesToMaybeKeep = from entry in wave[maxy][maxx]
+                                  where entry.Value
+                                  select entry.Key;
+            var values = from key in onesToMaybeKeep
+                        select patterns[key];
+            var total = values.Sum();
+
+            var oneToKeep = Random.Range(0, total-1);
+            int i = 0;
+            var patternToKeep = onesToMaybeKeep.ToList()[0];
+            foreach (var maybe in onesToMaybeKeep)
+            {
+                i += patterns[maybe];
+                if (i > oneToKeep)
+                {
+                    patternToKeep = maybe;
+                }
+            }
+
+
             foreach (var possibility in wave[maxy][maxx].Keys.ToArray())
             {
-                detectContradictions(wave, out _, out _, out _);
-                i++;
-                if (i >= oneToKeep && wave[maxy][maxx][possibility] && !alreadySet)
+                if (possibility == patternToKeep)
                 {
-                    alreadySet = true;
                     wave[maxy][maxx][possibility] = true;
-                    detectContradictions(wave, out _, out _, out _);
-                    continue;
                 }
-                wave[maxy][maxx][possibility] = false;
+                else
+                {
+                    wave[maxy][maxx][possibility] = false;
+                }
                 detectContradictions(wave, out _, out _, out _);
             }
 
             // propagate updates
-            var fdsaf = 0;
+            var gas = 50;
             while (true)
             {
                 detectContradictions(wave, out _, out _, out _);
-                fdsaf += 1;
+                gas -= 1;
                 bool madeAChange = false;
                 foreach (int y in Enumerable.Range(0, wave.Count))
                 {
                     foreach (int x in Enumerable.Range(0, wave[0].Count))
                     {
+                        int deletions = 0;
                         var truePossibilities = from entry in wave[y][x]
                                                 where entry.Value
                                                 select entry.Key;
-                        if (truePossibilities.Count() < 2)
+                        int c = truePossibilities.Count();
+                        if (c < 2)
                         {
                             continue;
                         }
@@ -230,15 +273,17 @@ public class WaveformcCollapse : MonoBehaviour
                             }
                             if (!possibilityValid)
                             {
+                                deletions += 1;
                                 detectContradictions(wave, out _, out _, out _);
                                 wave[y][x][possibility] = false;
                                 detectContradictions(wave, out _, out _, out _);
                                 madeAChange = true;
+                                if (contradictive) return;
                             }
                         }
                     }
                 }
-                if (!madeAChange || fdsaf > 100) break;
+                if (!madeAChange || gas < 1) break;
 
             }
 
@@ -304,4 +349,47 @@ public class WaveformcCollapse : MonoBehaviour
 
         }
     }
+
+    List<List<int>> transpose(List<List<int>> input)
+    {
+        var output = new List<List<int>>();
+
+        foreach (var value in input[0])
+        {
+            output.Add(new List<int>());
+        }
+
+        foreach (int y in Enumerable.Range(0, input.Count))
+        {
+            foreach (int x in Enumerable.Range(0, input[0].Count))
+            {
+                output[x].Add(input[y][x]);
+            }
+        }
+        return output;
+    }
+
+    List<List<int>> flip(List<List<int>> input)
+    {
+        var output = new List<List<int>>();
+        foreach (int y in Enumerable.Range(0, input.Count))
+        {
+            output.Add(input[y].AsEnumerable().Reverse().ToList());
+        }
+        return output;
+    }
+
+    List<List<int>> rotate90(List<List<int>> input)
+    {
+        return flip(transpose(input));
+    }
+    List<List<int>> rotate180(List<List<int>> input)
+    {
+        return rotate90(rotate90(input));
+    }
+    List<List<int>> rotate270(List<List<int>> input)
+    {
+        return rotate90(rotate90(rotate90(input)));
+    }
+
 }
